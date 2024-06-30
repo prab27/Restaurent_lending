651604f37fc9a56d256413adfa6332e581dadec1
prab27 <ayushsin72@gmail.com>
2024-06-30 12:52:20 +0530

    update

---
+++ src/theme.js
@@ -0,0 +1,13 @@
+const theme = {
+    colors: {
+      primary: '#5C9210',
+      primaryDark: '#4B7C08',
+      secondary: '#909090',
+      background: '#F7F7F7',
+      text: '#333',
+      white: '#FFF',
+    },
+  };
+  
+  export default theme;
+  
